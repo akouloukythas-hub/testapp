9c736e74b48759d3849157b89b94a9c4ff22fd05
akouloukythas-hub <akouloukythas@gmail.com>
2025-11-15 01:06:01 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+PRODUCTS = [
+    {"name": "Espresso", "price": 2.5},
+    {"name": "Cappuccino", "price": 3.0},
+    {"name": "Latte", "price": 3.5},
+    {"name": "Frappe", "price": 2.0},
+    {"name": "Πορτοκαλάδα", "price": 1.5}
+]
+
+DATABASE = "cafe.db"
